5592b508824368312eb70595d8732a34464735bb
Christon <christonja@gmail.com>
2019-06-16 09:47:35 +0800

    Update dburl

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost/yelp_camp_v13Deploy", { useNewUrlParser: true });
-mongoose.connect("mongodb+srv://Alternator:qKgJLL2GEfDzkBTu@cluster0-wkbxj.mongodb.net/yelp_camp?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+// mongoose.connect("mongodb+srv://Alternator:qKgJLL2GEfDzkBTu@cluster0-wkbxj.mongodb.net/yelp_camp?retryWrites=true&w=majority", { useNewUrlParser: true });
+// process.env.databaseURL
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -49,6 +52,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started");
 });
