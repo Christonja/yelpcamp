d414a3bf1f7b008ed65dfeba4745798a7d39c856
Christon <christonja@gmail.com>
2019-06-16 09:12:47 +0800

    Add Mongo Lab

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-
-mongoose.connect("mongodb://localhost/yelp_camp_v13", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/yelp_camp_v13Deploy", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://Alternator:qKgJLL2GEfDzkBTu@cluster0-wkbxj.mongodb.net/yelp_camp?retryWrites=true&w=majority", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
